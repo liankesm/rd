b7b31f9d1431e7e03fa66d6f249c835ef145c4ef
liankesm <119463268+liankesm@users.noreply.github.com>
2026-03-20 08:33:22 +0800

    Update RENDEZVOUS_SERVERS address

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -106,7 +106,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["v4.vt555.xyz"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["v4.vt55.xyz"];
 pub const RS_PUB_KEY: &str = "hTCJ8KNH9JkTJuUDLU5P9Iq6wnwTyqz5tGScR0Prx94=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
